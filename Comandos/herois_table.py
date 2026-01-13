5c6d3ad096e7bdff2579d9abb684e0c6bb03f841
Marcus Cruz <marcusvcb.nascimento@gmail.com>
2026-01-12 22:19:38 -0300

    feat: Row Number na vida dos herois por função

--- Comandos/herois_table.py
+++ Comandos/herois_table.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import sqlite3
 
-# Caminhos
 csv_path = "dados_ow.csv"
 db_path = "base_overwatch.db"
 
-# Conectar ao banco SQLite
 conn = sqlite3.connect(db_path)
 
-# Ler CSV
 df = pd.read_csv(csv_path, sep = ";")
 
-# Inserir no banco
 df.to_sql(
     "hero_info",
     conn,
@@ -19,7 +15,6 @@ df.to_sql(
     index=False
 )
 
-# Fechar conexão
 conn.close()
 
 print("✅ Dados importados com sucesso!")
